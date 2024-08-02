98cdc642b7174970c04578b03ebf7bc39f021cc8
xsni1 <thesniboy@gmail.com>
2024-08-02 21:14:35 +0200

    implement extraction of selected text

--- lua/mongo-grip-nvim/init.lua
+++ lua/mongo-grip-nvim/init.lua
@@ -21,7 +21,47 @@ end
 -- lua mongodb driver cannot be used as it lacks some important features.
 -- for example it doesn't allow us to `explain` the query
 --
--- if performance starts to be an issue, start a child process with mongosh and send queries to it via stdin
+-- if performance starts to be an issue, spawn a child process with mongosh and send queries to it via stdin
+
+local function get_selected_text()
+    local _, sel_start_line, sel_start_col = unpack(vim.fn.getpos("v"))
+    local _, sel_end_line, sel_end_col = unpack(vim.fn.getpos("."))
+    local mode = vim.api.nvim_get_mode().mode
+    local selected = {}
+
+    if mode == "v" then
+        if sel_start_line < sel_end_line then
+            selected = vim.api.nvim_buf_get_text(0, sel_start_line-1, sel_start_col-1, sel_end_line-1, sel_end_col, {})
+        else
+            selected = vim.api.nvim_buf_get_text(0, sel_end_line-1, sel_end_col-1, sel_start_line-1, sel_start_col, {})
+        end
+    end
+
+    if mode == "V" then
+        if sel_end_line > sel_start_line then
+            selected = vim.api.nvim_buf_get_lines(0, sel_start_line-1, sel_end_line, true)
+        else
+            selected = vim.api.nvim_buf_get_lines(0, sel_end_line-1, sel_start_line, true)
+        end
+    end
+
+    -- 22 / 0x16 - is string representation of Ctrl+v
+    if mode == "\x16" then
+        if sel_start_line > sel_end_line then
+            sel_start_line, sel_end_line = sel_end_line, sel_start_line
+        end
+
+        if sel_start_col > sel_end_col then
+            sel_start_col, sel_end_col = sel_end_col, sel_start_col
+        end
+
+        for i=sel_start_line, sel_end_line, 1 do
+            table.insert(selected, unpack(vim.api.nvim_buf_get_text(0, i-1, sel_start_col-1, i-1, sel_end_col, {})))
+        end
+    end
+
+    return selected
+end
 local function mongoConnect()
     local config = require("mongo-grip-nvim.config")
 
@@ -33,17 +73,25 @@ end
 
 local function query()
     local config = require("mongo-grip-nvim.config")
-    local result = vim.system({
-        "mongosh",
-        config.connString,
-        "--eval",
-        "db.getCollectionNames()"
-    }, { text = true }):wait()
+    local selected = get_selected_text()
 
-    print(result.stdout)
+    -- "v" and "." are used to determine "live" cursor position, :help line()
+
+    print("selected: ", table.concat(selected), #selected)
+    -- local result = vim.system({
+    --     "mongosh",
+    --     config.connString,
+    --     "--eval",
+    --     "db.getCollectionNames()"
+    -- }, { text = true }):wait()
+
+    -- print(result.stdout)
 end
+
 
 vim.api.nvim_create_user_command("MongoGripConnect", mongoConnect, {})
 vim.api.nvim_create_user_command("MongoGripQuery", query, {})
+
+vim.keymap.set({"v"}, "<leader>mq", query)
 
 return M
